cc085f38bebd551db865add4e8681521aea5e46a
Bohan Liu <bohanl@me.com>
2015-01-29 03:16:05 -0800

    update

---
+++ BinaryTreeInorderTraversal.py
@@ -0,0 +1,24 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of integers
+    def inorderTraversal(self, root):
+        curr, stack = root, []
+
+        res = []
+        while curr or len(stack) != 0:
+            if curr:
+                stack.append(curr)
+                curr = curr.left
+            else:
+                curr = stack.pop()
+                res.append(curr.val)
+                curr = curr.right
+
+        return res
