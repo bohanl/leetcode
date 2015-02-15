7f7234c0f592f8e597093b22b95958fa9f9fac6c
Bohan Liu <bohanl@me.com>
2015-02-15 13:50:20 -0800

    update

--- BalancedBinaryTree.py
+++ BalancedBinaryTree.py
@@ -8,11 +8,17 @@
 class Solution:
 
     def maxDepth(self, root):
-        if not root: return 0
+        if not root:
+            return 0
+
         L = self.maxDepth(root.left)
-        if L == -1: return -1
+        if L == -1:
+            return -1
+
         R = self.maxDepth(root.right)
-        if R == -1: return -1
+        if R == -1:
+            return -1
+
         return max(L, R)+1 if abs(L-R)<=1 else -1
 
     # @param root, a tree node
