98903bf1cc174842acd1b36b03926f49b6177ff9
Bohan Liu <bohanl@me.com>
2015-02-15 13:28:59 -0800

    update

--- ExcelSheetColumnTitle.py
+++ ExcelSheetColumnTitle.py
@@ -1,12 +1,10 @@
 class Solution:
     # @return a string
     def convertToTitle(self, num):
-        assert num > 0
-
-        q = collections.deque([])
+        s = ''
         while num:
             mod = True and num % 26 or 26
-            q.appendleft(chr(mod+64))
+            s = chr(mod+64) + s
             num = (num-mod)/ 26
 
-        return ''.join(q)
+        return s
