ce1e77e1d74079a8cea5e99ac5f961ae5226ed96
Bohan Liu <bohanl@me.com>
2015-02-17 22:53:59 -0800

    update

---
+++ BinarySearchTreeIterator.py
@@ -0,0 +1,27 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class BSTIterator:
+    # @param root, a binary search tree's root node
+    def __init__(self, root):
+        self.stack = []
+        while root:
+            self.stack.append(root)
+            root = root.left
+
+    # @return a boolean, whether we have a next smallest number
+    def hasNext(self):
+        return self.stack
+
+    # @return an integer, the next smallest number
+    def next(self):
+        curr_node = self.stack.pop()
+        node = curr_node.right
+        while node:
+            self.stack.append(node)
+            node = node.left
+        return curr_node.val
