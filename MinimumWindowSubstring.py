30a30017aac8197ef8fe2fe13eddf0852a66a6b8
Bohan Liu <bohanl@me.com>
2015-02-23 22:12:41 -0800

    update

--- MinimumWindowSubstring.py
+++ MinimumWindowSubstring.py
@@ -1,30 +1,34 @@
 class Solution:
     # @return a string
     def minWindow(self, S, T):
-        m, n = len(S), len(T)
-        if m < n: return ''
+        n, m = len(S), len(T)
+        if n < m: return ''
 
-        needed, found = [0]*256, [0]*256
+        found, needed = [0]*256, [0]*256
         for c in T: needed[ord(c)] += 1
-        
+
         start, count, min_window = 0, 0, ''
-        for end, ec in enumerate(S):
-            i = ord(ec)
-            if needed[i] == 0:
-                continue
-            if found[i] < needed[i]:
-                count += 1
-            found[i] += 1
-            if count != n:
-                continue
-            # move start
+        for end, sc in enumerate(S):
+            p = ord(sc)
+            # sc is not needed
+            if needed[p] == 0: continue
+            # needed and we haven't found all needed chars in S yet
+            if found[p] < needed[p]: count += 1
+            found[p] += 1
+            if count != m: continue
+
+            # when we get here, we know that we found at least all we need
+            # and now it's a good time to try to minimize the window
+            # move start point
             while start < end:
-                j = ord(S[start])
-                if needed[j] != 0:
-                    if found[j] <= needed[j]:
-                        break
-                    found[j] -= 1
+                p = ord(S[start])
+                if needed[p] != 0:
+                    # Can't go beyond this point since this is the minimal
+                    # we need to maintain the window
+                    if found[p] == needed[p]: break
+                    found[p] -= 1
                 start += 1
+
             # update result
             if not min_window or end-start+1 < len(min_window):
                 min_window = S[start:end+1]
