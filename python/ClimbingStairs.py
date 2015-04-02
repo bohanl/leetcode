8f3dbad689141cf7e729652457e220d670332d3d
Bohan Liu <bohanl@me.com>
2015-04-01 23:30:02 -0700

    update

--- python/ClimbingStairs.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    # @param n, an integer
-    # @return an integer
-    def climbStairs(self, n):
-        s1, s2 = 1, 1
-        for i in xrange(2,n+1):
-            s1, s2 = s2, s1+s2
-
-        return s2
