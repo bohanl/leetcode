8e49505e17ea0a9bc4712912a3c91470b4cb59d3
Bohan Liu <bohanl@me.com>
2015-04-01 23:47:27 -0700

    update

--- RepeatedDNASequences.java
+++ RepeatedDNASequences.java
@@ -1,20 +1,18 @@
 public class Solution {
-    private int compressStr(String s) {
+    
+    private static final Map<Character, Integer> compressMap = new HashMap<>();
+    
+    static {
+        compressMap.put('A', 0);
+        compressMap.put('C', 2);
+        compressMap.put('G', 1);
+        compressMap.put('T', 3);
+    }
+    
+    private int compressStr(String s, int start, int end) {
         int x = 0;
-        for (char c : s.toCharArray()) {
-            switch (c) {
-            case 'C': // 10
-                x |= 2;
-                break;
-            case 'G': // 01
-                x |= 1;
-                break;
-            case 'T': // 11
-                x |= 3;
-                break;
-            default: // 'A' 00
-                break;
-            }
+        for (int i = start; i < end; i++) {
+            x |= compressMap.get(s.charAt(i));
             x <<= 2;
         }
         
@@ -22,19 +20,21 @@ public class Solution {
     }
     
     public List<String> findRepeatedDnaSequences(String s) {
-        Set<String> res = new HashSet<String>();
-        Set<Integer> set = new HashSet<Integer>();
+        List<String> res = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
         
         for (int i = 0; i <= s.length()-10; i++) {
-            String ss = s.substring(i, i+10);
-            int v = compressStr(ss);
-            if (set.contains(v)) {
-                res.add(ss);
-            } else {
-                set.add(v);
+            int v = compressStr(s, i, i+10);
+            int count = 1;
+            if (map.containsKey(v)) {
+                count += map.get(v);
             }
+            if (count == 2) {
+                res.add(s.substring(i, i+10));
+            }
+            map.put(v, count);
         }
         
-        return new ArrayList<String>(res);
+        return res;
     }
 }
