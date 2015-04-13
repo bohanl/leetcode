55f46cefc36d5cf527b2111dde4ecdc138081de9
Bohan Liu <bohan.a.liu@gmail.com>
2015-04-12 23:52:52 -0700

    update

---
+++ SumRootToLeafNumbers.py
@@ -0,0 +1,25 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def backtrack(self, node, a, arr):
+        if not node:
+            return
+        a = a*10 + node.val
+        if not node.left and not node.right:
+            arr.append(a)
+        if node.left:
+            self.backtrack(node.left, a, arr)
+        if node.right:
+            self.backtrack(node.right, a, arr)
+
+    # @param root, a tree node
+    # @return an integer
+    def sumNumbers(self, root):
+        a, arr = 0, []
+        self.backtrack(root, a, arr)
+        return sum(arr)
