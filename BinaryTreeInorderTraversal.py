0163f87d1692bc8d00ff5d48efa409caa9d57778
Bohan Liu <bohan.a.liu@gmail.com>
2015-04-13 23:40:50 -0700

    update

--- BinaryTreeInorderTraversal.py
+++ BinaryTreeInorderTraversal.py
@@ -15,9 +15,9 @@ class Solution:
             if curr:
                 stack.append(curr)
                 curr = curr.left
-            else:
-                curr = stack.pop()
-                res.append(curr.val)
-                curr = curr.right
+                continue
+            curr = stack.pop()
+            res.append(curr.val)
+            curr = curr.right
 
         return res
